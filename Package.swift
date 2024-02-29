f3e7f5986a9cf71026fa4225ef2d0af0586dd5ce
Shubham Gupta <108867424+shubham-gupta-zomato@users.noreply.github.com>
2024-02-29 11:55:40 +0530

    Automatic linking

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "google-maps-ios-utils",
-            type: .dynamic,
             targets: ["google-maps-ios-utils"]
         )
     ],
